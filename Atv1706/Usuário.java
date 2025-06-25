b534e6339a87c30aaa4e19d99d92b7a738071eb4
Caioba123 <caiomuller7192@gmail.com>
2025-06-25 07:30:43 -0300

    Update Usuário.java

--- Atv1706/Usuário.java
+++ Atv1706/Usuário.java
@@ -16,14 +16,21 @@ public class Usuário {
          System.out.println("Olá, Bem-vindo");
          System.out.println("Se cadastre no nosso sistema de assinatura!");
          
-         Scanner tec = new Scanner(System.in);
-         System.out.println("Diga seu nome");
-         String nome = tec.nextLine();
-         System.out.println("Plano Free:");
-         System.out.println("Plano Premium:");
-         System.out.println("Plano Premium:");
-         System.out.println("Diga o plano que voce deseja");
-         String plano = tec.nextLine();
+            System.out.println("Digite o seu nome:");
+        String nome = scanner.nextLine();  
+
+        System.out.println("Digite o seu email:");
+        String email = scanner.nextLine(); 
+
+        System.out.println("Digite a sua senha:");
+        int senha = scanner.nextInt(); 
+
+        System.out.println("Digite o tipo de usuario, se e comum ou ADM:");
+         String tipo = scanner.nextLine();
+         
+         System.out.println("Digite a sua data de inscriçao:");
+         int data = scanner.nextInt();
+
          
 }
 }
